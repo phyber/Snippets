3c4f2434c8f654af29c1d2823fcfc1c7f299e499
phyber <phyber@localhost>
2010-04-13 16:12:47 +0100

    Lua: Added procuptime.lua, a little more than a snippet, but meh.

---
+++ Lua/procuptime.lua
@@ -0,0 +1,79 @@
+#!/usr/bin/env lua
+--[[
+-- Shows the uptime of a given PID.
+--]]
+-- Required for stat()
+require "posix"
+
+-- Location of the uptime file and then
+-- format string for the stat files.
+local procuptime = "/proc/uptime"
+local procpidstatfmt = "/proc/%d/stat"
+
+-- Small wrapper for posix.stat, since we only care about filetype.
+function filetype(f)
+	return posix.stat(f, "type")
+end
+
+-- Split a string like Perl's split.
+-- Returns a table.
+function split(str, sep)
+	local t = {}
+	local pattern = "(.-)"..sep
+	local last_end = 1
+	local s, e, cap = str:find(pattern, 1)
+	while s do
+		if s ~= 1 or cap ~= "" then
+			table.insert(t, cap)
+		end
+		last_end = e + 1
+		s, e, cap = str:find(pattern, last_end)
+	end
+	if last_end <= #str then
+		cap = str:sub(last_end)
+		table.insert(t, cap)
+	end
+	return t
+end
+o-- Reads the uptime from the procuptime path givena bove.
+function get_sys_uptime()
+	local uptimefile = assert(io.open(procuptime, "rb"))
+	local content = split(uptimefile:read("*all"), " ")[1]
+	io.close(uptimefile)
+
+	return content
+end
+
+-- Get the PIDs uptime along with its name.
+-- Returns a list.
+function get_proc_uptime(pid)
+	local procfile = assert(io.open(string.format(procpidstatfmt, pid), "rb"))
+	local content = split(procfile:read("*all"), " ")
+	io.close(procfile)
+
+	local uptime = content[22]
+	local procname = content[2]
+
+	return uptime, procname
+end
+
+-- Shows the uptime duration in a nice string.
+function duration(uptime)
+	local interval = {}
+	local str
+	for _, i in ipairs({60, 60, 24, 365}) do
+		table.insert(interval, uptime % i)
+		uptime = math.floor(uptime / i)
+	end
+	str = string.format("%dy %dd %dh %dm %ds", uptime, interval[4], interval[3], interval[2], interval[1])
+	return str
+end
+
+local pid = arg[1]
+
+if filetype(string.format(procpidstatfmt, pid)) == "regular" then
+	local procuptime, procname = get_proc_uptime(pid)
+	print(string.format("PID: %d  %s -> %s", pid, procname, duration(get_sys_uptime() - (procuptime / 100))))
+else
+	print(string.format("Error. PID %d none existant or something went terribly wrong.", pid))
+end
